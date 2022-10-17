2f40c5dcfbc5abe6c8004f3310cf5b7272384a3d
EduardoMorales <eduardo@inmo-puebla.com>
2022-10-17 11:09:27 -0500

    se añade introduccion de ocmo usar la comunicacion con netlify " sintaxis predetermianda para siempre usar siempr eeatrings y usar un statud code y el string en el body-"

---
+++ functions/1-Hello.js
@@ -0,0 +1,33 @@
+// domain :  domain/.netlify/functions/1-.Hello
+
+const { json } = require("stream/consumers");
+
+// using strinfy with an object or arrya.------------
+
+const person = {
+  name: "Eduardo",
+  profesion: { abogado: { tipo: "penal", tipo2: "mercantil" } },
+};
+
+// .-------------------------------------------------
+// !! always string
+//  1.---------HERE QE USE A ASYNC FUNCTION .----------------------
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: "Esta es mi primer funcion en Netlify ",
+
+    // JSON.stringlify
+    // body: JSON.stringify(person),
+  };
+};
+
+// 2.--------USING A CALL BACK.-----------------------
+
+// exports.handler = (event, context, callback) => {
+//   callback(null, {
+//     statusCode: 200,
+//     body: "using a callback as second form of communication. ",
+//   });
+// };
+// -------------------------------------------------
